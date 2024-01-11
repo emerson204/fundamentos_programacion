0e069a2a8e2a2bdea5bf97364504152fd8f15f69
Emerson <emecasti65@gmail.com>
2024-01-10 23:02:23 -0500

    Aplicando resumen sobre fundamentos de programacion

---
+++ app.js
@@ -0,0 +1,37 @@
+//Variables
+let nombre = "Aldahir";
+const valorUnico = "Soy un valor no reasignable";
+
+//Tipos de datos
+let entero = 20;
+let flotantes = 10.5;
+let string = "Soy un string";
+let boleano = true;
+
+//Operadores
+
+//Operadores aritmeticos
+let a = 5;
+let b = 2;
+let suma = a + b; // 7
+let resta = a - b; //3
+let multiplicacion = a * b; // 10
+let division = a / b; // 2.5
+let modulo = a % b; // 1
+let exponente = a ** b; // 25
+
+//Operadores de asignacion
+let asignacion = "Con el = le asignamos este valor a la variable asignacion";
+
+//Operadores de comparacion
+a = 10;
+b = 8;
+es_igual = a < b; // False
+mayor_que = a > b; // True
+
+//Operadores logicos
+let condicion1 = true;
+let condicion2 = false;
+
+let resultadoAnd = condicion1 && condicion2; // false (verdadero si ambas condiciones son verdaderas)
+let resultadoOr = condicion1 || condicion2; // true (verdadero si al menos una condición es verdadera)
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Que es la programacion
+
+La programación es el proceso de diseñar e implementar un conjunto de instrucciones que le dicen a una computadora cómo realizar una tarea específica
+
+# Que son las variables
+
+Las variables son como unas cajas que se almacenan datos
+Existen las constantes la cual sus valores no pueden ser reasignados
+
+# Que son los tipos de datos
+
+Los tipos de datos son los que se almacenan en las variables , pues existen tipos de datos como : enteros(20) , booleanos (true o false) , string("Cadenas de texto") , flotantes(50.32)
+
+# Que son los operadores y definir con ejemplos todos los vistos en clase
+
+Pues primero tenemos operadores aritmeticos que son : (Suma resta multiplicacion division )
+Luego tenemos operadores de asignacion (=)
+Operadores de comparacion (<,>,<=,>=)
+Operadores logicos (|| , &&)
